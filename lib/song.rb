eccd46d30a2dc5ef295a5021df4a45c9ef4f21f7
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-19 18:13:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,11 +29,11 @@ class Song
     song 
   end 
   
-  def self.find_by_name
+  def self.find_by_name(name)
     @@all.find {|song| song.name == name }
   end 
   
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(name)
     if self.find_by_name(name)
       self.find_by_name
     else 
