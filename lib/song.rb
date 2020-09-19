96b6ead1a7f727263efba9b45a80e25f751b1daf
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-19 17:28:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,38 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+     self.new 
+      
+    end 
+    name 
+  end 
+  
+  def self.new_by_name
+    new_by_name = name 
+  end 
+  
+  def self.create_by_name
+  end 
+  
+  def self.find_by_name
+  end 
+  
+  def self.find_or_create_by_name
+  end 
+  
+  def self.alphabetical 
+    @@all.sort {|a,b| a<=>b }
+  end 
+  
+  def self.new_from_file
+  end 
+  
+  def self.create_from_file 
+  end 
+  
+  def self.destroy_all
+    @@all.clear
+  end 
+
 end
