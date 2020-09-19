d4ee2152cbd23ca621a69c19be18e8eeba88ad0b
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-19 18:09:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,6 +34,11 @@ class Song
   end 
   
   def self.find_or_create_by_name
+    if self.find_by_name(name)
+      self.find_by_name
+    else 
+      self.create_by_name(name)
+      self.create_by_name
   end 
   
   def self.alphabetical 
