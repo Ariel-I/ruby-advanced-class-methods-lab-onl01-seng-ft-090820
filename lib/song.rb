a409735b519d1741f5bc3e16719a748878ae1087
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-19 19:09:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,8 @@ class Song
    def self.new_from_filename(filename)
     split_filename = filename.chomp(".mp3").split("-")
     song = song.new 
-    song.new 
+    song.name = split_filename[1]
+    song.artist_name = split_filename[0]
   end 
   
   def self.create_from_filename(filename) 
