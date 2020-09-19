ec2c8d3d794e01f69f521f595984b2ffb0ecf0b7
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-19 17:53:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,9 +19,11 @@ class Song
   def self.new_by_name(name)
      song = song.new 
      song.name = name 
+     song 
   end 
   
   def self.create_by_name
+    
   end 
   
   def self.find_by_name
